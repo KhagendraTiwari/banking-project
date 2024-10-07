2c215c4c8b89bf121e17b2f0f0062a5f8184b982
KhagendraTiwari <162550347+KhagendraTiwari@users.noreply.github.com>
2024-10-07 07:49:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,21 +11,7 @@ resource "aws_instance" "testserver" {
     Name = "testserver"
   }
 }
-
-resource "aws_instance" "prodserver" {
-  ami           = "ami-0866a3c8686eaeeba"  # Replace with your desired AMI ID
-  instance_type = "t2.micro"      # Choose your instance type
-  key_name      = "ktkey" # Replace with your key pair name
-
-  tags = {
-    Name = "prodserver"
-  }
-}
-
 output "testserver_public_ip" {
   value = aws_instance.testserver.public_ip
 }
 
-output "prodserver_public_ip" {
-  value = aws_instance.prodserver.public_ip
-}
