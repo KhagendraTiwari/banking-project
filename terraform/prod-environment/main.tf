b9ce42ba76019a3064f7b964f3f5020469376ebc
KhagendraTiwari <162550347+KhagendraTiwari@users.noreply.github.com>
2024-10-07 12:09:31 +0530

    Update main.tf

--- terraform/prod-environment/main.tf
+++ terraform/prod-environment/main.tf
@@ -2,7 +2,17 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Data source to check if the production instance already exists
+data "aws_instance" "existing_prodserver" {
+  # Use a filter to find an existing instance by its name tag
+  filter {
+    name   = "tag:Name"
+    values = ["prodserver"]
+  }
+}
+
 resource "aws_instance" "prodserver" {
+  count         = length(data.aws_instance.existing_prodserver.*.id) == 0 ? 1 : 0  # Create only if it doesn't exist
   ami           = "ami-0866a3c8686eaeeba"  # Replace with your desired AMI ID
   instance_type = "t2.micro"                # Choose your instance type
   key_name      = "ktkey"                   # Replace with your key pair name
@@ -13,5 +23,5 @@ resource "aws_instance" "prodserver" {
 }
 
 output "prodserver_public_ip" {
-  value = aws_instance.prodserver.public_ip
+  value = aws_instance.prodserver[0].public_ip  # Access the public IP of the instance
 }
