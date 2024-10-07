479fd734fa6f39528742a52c145b8ae809ee81e4
KhagendraTiwari <162550347+KhagendraTiwari@users.noreply.github.com>
2024-10-07 07:42:25 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = "us-east-1"  # Update the region if necessary
+}
+
+resource "aws_instance" "testserver" {
+  ami           = "ami-0866a3c8686eaeeba"  # Replace with your desired AMI ID
+  instance_type = "t2.micro"      # Choose your instance type
+  key_name      = "ktkey" # Replace with your key pair name
+
+  tags = {
+    Name = "testserver"
+  }
+}
+
+resource "aws_instance" "prodserver" {
+  ami           = "ami-0866a3c8686eaeeba"  # Replace with your desired AMI ID
+  instance_type = "t2.micro"      # Choose your instance type
+  key_name      = "ktkey" # Replace with your key pair name
+
+  tags = {
+    Name = "prodserver"
+  }
+}
+
+output "testserver_public_ip" {
+  value = aws_instance.testserver.public_ip
+}
+
+output "prodserver_public_ip" {
+  value = aws_instance.prodserver.public_ip
+}
