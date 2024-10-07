cf032be437f30e55e7d29d93b9fcd33786f294e6
KhagendraTiwari <162550347+KhagendraTiwari@users.noreply.github.com>
2024-10-07 12:47:31 +0530

    Update main.tf

--- terraform/test-environment/main.tf
+++ terraform/test-environment/main.tf
@@ -2,26 +2,25 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Data source to check if the test instance already exists
-data "aws_instance" "existing_testserver" {
-  # Use a filter to find an existing instance by its name tag
+# Data source to check if the production instance already exists
+data "aws_instances" "existing_prodserver" {
   filter {
     name   = "tag:Name"
-    values = ["testserver"]
+    values = ["prodserver"]
   }
 }
 
-resource "aws_instance" "testserver" {
-  count         = length(data.aws_instance.existing_testserver.*.id) == 0 ? 1 : 0  # Create only if it doesn't exist
+resource "aws_instance" "prodserver" {
+  count         = length(data.aws_instances.existing_prodserver.ids) == 0 ? 1 : 0  # Create only if it doesn't exist
   ami           = "ami-0866a3c8686eaeeba"  # Replace with your desired AMI ID
   instance_type = "t2.micro"                # Choose your instance type
   key_name      = "ktkey"                   # Replace with your key pair name
 
   tags = {
-    Name = "testserver"
+    Name = "prodserver"
   }
 }
 
-output "testserver_public_ip" {
-  value = length(aws_instance.testserver) > 0 ? aws_instance.testserver[0].public_ip : "No instance created"  # Handle case when instance does not exist
+output "prodserver_public_ip" {
+  value = length(aws_instance.prodserver) > 0 ? aws_instance.prodserver[0].public_ip : "No instance created"  # Handle case when instance does not exist
 }
