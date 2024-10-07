fee4fdf39172536d1f9434514f24b579ebb678ae
KhagendraTiwari <162550347+KhagendraTiwari@users.noreply.github.com>
2024-10-07 12:08:16 +0530

    Update main.tf

--- terraform/test-environment/main.tf
+++ terraform/test-environment/main.tf
@@ -2,7 +2,17 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Data source to check if the instance already exists
+data "aws_instance" "existing_testserver" {
+  # Use a filter to find an existing instance by its name tag
+  filter {
+    name   = "tag:Name"
+    values = ["testserver"]
+  }
+}
+
 resource "aws_instance" "testserver" {
+  count         = length(data.aws_instance.existing_testserver.*.id) == 0 ? 1 : 0  # Create only if it doesn't exist
   ami           = "ami-0866a3c8686eaeeba"  # Replace with your desired AMI ID
   instance_type = "t2.micro"                # Choose your instance type
   key_name      = "ktkey"                   # Replace with your key pair name
@@ -13,5 +23,5 @@ resource "aws_instance" "testserver" {
 }
 
 output "testserver_public_ip" {
-  value = aws_instance.testserver.public_ip
+  value = aws_instance.testserver[0].public_ip  # Access the public IP of the instance
 }
