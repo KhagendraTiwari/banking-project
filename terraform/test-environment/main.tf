98e6d458a7c11c2ab315d507073c47574023afed
KhagendraTiwari <162550347+KhagendraTiwari@users.noreply.github.com>
2024-10-14 09:05:14 +0530

    Update main.tf

--- terraform/test-environment/main.tf
+++ terraform/test-environment/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
 data "aws_instances" "existing_testserver" {
@@ -12,7 +12,7 @@ data "aws_instances" "existing_testserver" {
 resource "aws_instance" "testserver" {
   count = length(data.aws_instances.existing_testserver.ids) == 0 ? 1 : 0
 
-  ami           = "ami-12345678"  # Replace with actual AMI ID
+  ami           = "ami-0ea3c35c5c3284d82"  # Replace with actual AMI ID
   instance_type = "t2.micro"
 
   tags = {
